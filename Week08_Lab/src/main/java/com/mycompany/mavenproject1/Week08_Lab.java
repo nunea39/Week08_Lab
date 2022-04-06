d78c6406735b665dcfbdba41008dea2e689db54c
domin <domin@137.125.154.25>
2022-04-06 13:23:45 -0400

    Comments commit

---
+++ Week08_Lab/src/main/java/com/mycompany/mavenproject1/Week08_Lab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ * Write an algorithm to find the sum of the prime numbers which have the digit 
+ * '3' in between 0 to n (n should be less than 1,000,000).
+ * For example, the sum of the prime numbers (with 3 digit) 
+ * between 0 and n=100 is (3+ 13+  23+ 31+ 37+ 43+  53+73+83 )= 359.
+ * @author domin
+ */
+public class Week08_Lab {
+    
+}
